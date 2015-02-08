f4f7204635f3b52ff7e84d2b958815807bafdf03
Neal Wu <nwu@box.com>
2015-02-07 20:18:51 -0800

    Movie detail view now has a janky label along with the image

--- Rotten Tomatoes/MovieDetailViewController.m
+++ Rotten Tomatoes/MovieDetailViewController.m
@@ -12,6 +12,7 @@
 @interface MovieDetailViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
 
 @end
 
@@ -23,6 +24,8 @@
     NSString *url = [self.movie valueForKeyPath:@"posters.thumbnail"];
     url = [url stringByReplacingOccurrencesOfString:@"tmb" withString:@"ori"];
     [self.posterView setImageWithURL:[NSURL URLWithString:url] placeholderImage:self.lowResImage];
+
+    self.synopsisLabel.text = self.movie[@"synopsis"];
 }
 
 @end
